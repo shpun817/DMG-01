d452a868f00bf521cd135e7df2458e27e7405607
Sanford Pun <punsanford@gmail.com>
2025-02-16 19:24:11 +0000

    Instruction Execution and Control Flow

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,14 +1,43 @@
 use crate::{
-    instruction::{ArithmeticTarget, Instruction},
+    instruction::{ArithmeticTarget, IncDecTarget, Instruction, JumpTest},
     register::Registers,
 };
 
 struct CPU {
     registers: Registers,
+    pc: u16,
+    bus: MemoryBus,
+}
+
+struct MemoryBus {
+    memory: [u8; 0xFFFF],
 }
 
 impl CPU {
-    fn execute(&mut self, instruction: Instruction) {
+    fn step(&mut self) {
+        let mut instruction_byte = self.bus.read_byte(self.pc);
+
+        let is_prefixed = instruction_byte == 0xCB;
+        if is_prefixed {
+            instruction_byte = self.bus.read_byte(self.pc + 1);
+        }
+
+        let next_pc =
+            if let Some(instruction) = Instruction::from_byte(instruction_byte, is_prefixed) {
+                self.execute(instruction)
+            } else {
+                let description = format!(
+                    "0x{}{:X}",
+                    if is_prefixed { "cb" } else { "" },
+                    instruction_byte
+                );
+                panic!("Unkown instruction found for: {}", description)
+            };
+
+        self.pc = next_pc;
+    }
+
+    fn execute(&mut self, instruction: Instruction) -> u16 {
         match instruction {
             Instruction::ADD(target) => match target {
                 ArithmeticTarget::A => todo!(),
@@ -17,12 +46,28 @@ impl CPU {
                     let value = self.registers.c;
                     let new_value = self.add(value);
                     self.registers.a = new_value;
+                    self.pc.wrapping_add(1)
                 }
                 ArithmeticTarget::D => todo!(),
                 ArithmeticTarget::E => todo!(),
                 ArithmeticTarget::H => todo!(),
                 ArithmeticTarget::L => todo!(),
             },
+            Instruction::INC(target) => match target {
+                IncDecTarget::BC => todo!(),
+                IncDecTarget::DE => todo!(),
+            },
+            Instruction::JP(test) => {
+                let jump_condition = match test {
+                    JumpTest::NotZero => !self.registers.f.zero,
+                    JumpTest::NotCarry => !self.registers.f.carry,
+                    JumpTest::Zero => self.registers.f.zero,
+                    JumpTest::Carry => self.registers.f.carry,
+                    JumpTest::Always => true,
+                };
+                self.jump(jump_condition)
+            }
+            _ => todo!(),
         }
     }
 
@@ -35,5 +80,26 @@ impl CPU {
         self.registers.f.half_carry = ((self.registers.a & 0xF) + (value & 0xF)) > 0xF;
 
         new_value
+    }
+
+    fn jump(&mut self, should_jump: bool) -> u16 {
+        if should_jump {
+            // Gameboy is little endian so read pc + 2 as most significant bit
+            // and pc + 1 as least significant bit
+            let least_significant_byte = self.bus.read_byte(self.pc + 1) as u16;
+            let most_significant_byte = self.bus.read_byte(self.pc + 2) as u16;
+            (most_significant_byte << 8) | least_significant_byte
+        } else {
+            // If we don't jump we need to still move the program
+            // counter forward by 3 since the jump instruction is
+            // 3 bytes wide (1 byte for tag and 2 bytes for jump address)
+            self.pc.wrapping_add(3)
+        }
+    }
+}
+
+impl MemoryBus {
+    fn read_byte(&self, address: u16) -> u8 {
+        self.memory[address as usize]
     }
 }
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,5 +1,8 @@
 pub(crate) enum Instruction {
     ADD(ArithmeticTarget),
+    INC(IncDecTarget),
+    RLC(PrefixTarget),
+    JP(JumpTest),
 }
 
 pub(crate) enum ArithmeticTarget {
@@ -10,4 +13,54 @@ pub(crate) enum ArithmeticTarget {
     E,
     H,
     L,
+}
+
+pub(crate) enum IncDecTarget {
+    BC,
+    DE,
+}
+
+pub(crate) enum PrefixTarget {
+    B,
+}
+
+pub(crate) enum JumpTest {
+    NotZero,
+    Zero,
+    NotCarry,
+    Carry,
+    Always,
+}
+
+impl Instruction {
+    pub(crate) fn from_byte(byte: u8, is_prefixed: bool) -> Option<Self> {
+        if is_prefixed {
+            Self::from_byte_prefixed(byte)
+        } else {
+            Self::from_byte_not_prefixed(byte)
+        }
+    }
+
+    fn from_byte_prefixed(byte: u8) -> Option<Self> {
+        match byte {
+            0x00 => Some(Self::RLC(PrefixTarget::B)),
+            _ =>
+            /* TODO: Add mapping for rest of instructions */
+            {
+                None
+            }
+        }
+    }
+
+    fn from_byte_not_prefixed(byte: u8) -> Option<Self> {
+        match byte {
+            0x02 => Some(Self::INC(IncDecTarget::BC)),
+            0x13 => Some(Self::INC(IncDecTarget::DE)),
+            _ =>
+            /* TODO: Add mapping for rest of instructions */
+            {
+                None
+            }
+        }
+    }
 }
